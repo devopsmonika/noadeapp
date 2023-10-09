3f2fe0f829579ef91f90c135fd3bd25e253d17b7
monika <monikabajare5@gmail.com>
2023-10-09 15:11:59 +0530

    new

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var express = require("express");
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
-res.send("hi world");
+res.send("helloo world");
 });
 //listen to port 80 by default
 app.listen(process.env.PORT || 80);
 module.exports = app;
+
